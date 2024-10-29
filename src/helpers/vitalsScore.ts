4db83195a8bd58b3f48f3f94f5e9b00780ccf9eb
Diana Tang <1987567948@qq.com>
2024-10-29 22:35:47 +0800

    feat: Set performance parameters: fcp,lcp,fid,cls,tbt and other parameter ranges

---
+++ src/helpers/vitalsScore.ts
@@ -0,0 +1,43 @@
+/*
+ * @Author: Diana Tang
+ * @Date: 2024-10-29 21:59:24
+ * @LastEditors: Diana Tang
+ * @Description: some description
+ * @FilePath: /DT-performance-monitor-SDK/src/helpers/vitalsScore.ts
+ */
+import { IDianaTangData, IVitalsScore } from '../typings/types';
+
+//https://web.dev/vitals/
+const fcpScore = [1000, 2500];
+const lcpScore = [2500, 4000];
+const fidcore = [100, 300];
+const clsScore = [0.1, 0.25];
+const tbtScore = [300, 600];
+
+export const webVitalsScore: Record<string, number[]> = {
+  fp: fcpScore,
+  fcp: fcpScore,
+  lcp: lcpScore,
+  lcpFinal: lcpScore,
+  fid: fidcore,
+  fidVitals: fidcore,
+  cls: clsScore,
+  clsFinal: clsScore,
+  tbt: tbtScore,
+  tbt5S: tbtScore,
+  tbt10S: tbtScore,
+  tbtFinal: tbtScore,
+};
+
+export const getVitalsScore = (
+  measureName: string,
+  value: IDianaTangData
+): IVitalsScore => {
+  if (!webVitalsScore[measureName]) {
+    return null;
+  }
+  if (value <= webVitalsScore[measureName][0]) {
+    return 'good';
+  }
+  return value <= webVitalsScore[measureName][1] ? 'needsImprovement' : 'poor';
+};
