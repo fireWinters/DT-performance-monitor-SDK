e9bff8b08dfd44e8b307d4bb67bcdfae0bab54a7
Diana Tang <1987567948@qq.com>
2024-10-31 18:24:26 +0800

    feat:Compatible with different browsers PerformanceObserver

---
+++ src/helpers/isSupported.ts
@@ -0,0 +1,21 @@
+/*
+ * @Author: Diana Tang
+ * @Date: 2024-10-31 17:38:29
+ * @LastEditors: Diana Tang
+ * @Description: some description
+ * @FilePath: /DT-performance-monitor-SDK/src/helpers/isSupported.ts
+ */
+import { W, WP } from '../data/constants';
+
+/**
+ * True if the browser supports the Navigation Timing API,
+ * User Timing API and the PerformanceObserver Interface.
+ * In Safari, the User Timing API (performance.mark()) is not available,
+ * so the DevTools timeline will not be annotated with marks.
+ * Support: developer.mozilla.org/en-US/docs/Web/API/Performance/mark
+ * Support: developer.mozilla.org/en-US/docs/Web/API/PerformanceObserver
+ * Support: developer.mozilla.org/en-US/docs/Web/API/Performance/getEntriesByType
+ */
+export const isPerformanceSupported = (): boolean => {
+  return WP && !!WP.getEntriesByType && !!WP.now && !!WP.mark;
+};
