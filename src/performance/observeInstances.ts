eef648914d8b0ab85f24eae76ea65991c3685c16
Diana Tang <1987567948@qq.com>
2024-11-01 15:50:16 +0800

    feat: add perfObservers

---
+++ src/performance/observeInstances.ts
@@ -0,0 +1,14 @@
+/*
+ * @Author: Diana Tang
+ * @Date: 2024-11-01 14:25:52
+ * @LastEditors: Diana Tang
+ * @Description: some description
+ * @FilePath: /DT-performance-monitor-SDK/src/performance/observeInstances.ts
+ */
+import { IPerfObservers } from '../typings/types';
+
+/**
+ * @remarks 核心性能数据指标对象
+ * @public
+ */
+export const perfObservers: IPerfObservers = {};
