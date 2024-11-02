d8ce1a5532a0b63f0031f9ac8cefe2a85ad36362
Diana Tang <1987567948@qq.com>
2024-11-03 00:13:49 +0800

    feat: add logMetric function

--- src/data/log.ts
+++ src/data/log.ts
@@ -7,6 +7,7 @@
  */
 import { reportPerf } from '../data/reportPerf';
 import { roundByTwo } from '../helpers/utils';
+import { config } from '../config';
 
   export const logData = (
     measureName: string,
@@ -23,3 +24,18 @@ import { roundByTwo } from '../helpers/utils';
      // Sends the metric to an external tracking service
     reportPerf(measureName, metric, customProperties);
   };
+  /**
+ * Dispatches the metric duration into internal logs
+ * and the external time tracking service.
+ */
+export const logMetric = (
+  duration: number,
+  measureName: string,
+  customProperties?: object
+): void => {
+  const duration2Decimal = roundByTwo(duration);
+  if (duration2Decimal <= config.maxTime && duration2Decimal >= 0) {
+    // 从内部或者外部的报告工具报告指标数据
+    reportPerf(measureName, duration2Decimal, customProperties);
+  }
+};
